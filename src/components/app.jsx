a8d320ea9b21577ba7c15dfd600ff4973b0042d8
ole dening <ole.dening@gmail.com>
2019-06-15 14:05:50 +0200

    did my first react app

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,11 +1,49 @@
 import React, {Component} from 'react';
+import giphy from 'giphy-api';
+
+import SearchBar from './search_bar.jsx';
+import Gif from './gif.jsx';
+import GifList from './gif_list';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gifs: [],
+      selectedGifId: "AaosjRHKjEcXm",
+    }
+
+    this.search("homer thinking");
+  }
+
+  search = (query) => {
+    giphy('YgSB7LLhIaIQ3BnSS6XLycIbJ4Jf8tcw').search({
+      q: query,
+      rating: 'g',
+      limit: 10,
+    }, // change the function below to an arrow function for the callback
+    (error, result) => {
+      this.setState({
+        gifs: result.data
+      });
+
+});
+
+  }
+    // E.g. we pass props 'searchFunction' to SearchBar
   render () {
     return (
       <div>
-         <div className="left-scene"></div>
-         <div className="right-scene"></div>
+         <div className="left-scene">
+            <SearchBar searchFunction={this.search}/>
+            <div className="selected-gif">
+              <Gif id={this.state.selectedGifId} />
+            </div>
+         </div>
+         <div className="right-scene">
+            <GifList gifs={this.state.gifs} />
+         </div>
       </div>
       );
   }
